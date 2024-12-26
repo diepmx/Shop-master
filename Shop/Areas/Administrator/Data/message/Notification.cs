cca5653b1a8930d5d0bfcbba87f820f7ea2b66f4
Mai Xuan Diep <diepdiep0901@gmail.com>
2024-12-26 12:48:07 +0700

    Sửa lỗi thông báo trả về null dẫn tới không chuyển hướng tới Views

--- Shop/Areas/Administrator/Data/message/Notification.cs
+++ Shop/Areas/Administrator/Data/message/Notification.cs
@@ -1,49 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 
 namespace Shop.Areas.Administrator.Data.message
 {
     public class Notification
     {
-        // Kiểm tra nếu Session["Notification"] tồn tại và không null
         public static bool has_flash()
         {
-            return HttpContext.Current.Session["Notification"] != null;
-        }
-
-        // Thiết lập một thông báo mới vào Session
-        public static void set_flash(string mgs, string mgs_type)
-        {
-            // Tạo một đối tượng ModelNotification
-            ModelNotification tb = new ModelNotification
+            if (HttpContext.Current.Session["Notification"].Equals(""))
             {
-                mgs = mgs,
-                mgs_type = mgs_type
-            };
+                return false;
+            }
+            return true;
+        }
+        public static void set_flash(String mgs, String mgs_type)
+        {
+            ModelNotification tb = new ModelNotification();
+            tb.mgs = mgs;
+            tb.mgs_type = mgs_type;
 
-            // Lưu đối tượng vào session
             HttpContext.Current.Session["Notification"] = tb;
         }
-
-        // Lấy thông báo từ Session và xóa sau khi sử dụng
         public static ModelNotification get_flash()
         {
-            // Kiểm tra nếu Session không chứa đối tượng ModelNotification
-            if (HttpContext.Current.Session["Notification"] == null || !(HttpContext.Current.Session["Notification"] is ModelNotification))
-            {
-                return null; // Trả về null nếu không hợp lệ
-            }
 
-            // Lấy đối tượng từ session
             ModelNotification Notifi = (ModelNotification)HttpContext.Current.Session["Notification"];
-
-            // Xóa thông báo sau khi sử dụng
-            HttpContext.Current.Session["Notification"] = null;
-
+            HttpContext.Current.Session["Notification"] = "";
             return Notifi;
         }
     }
-
-    // Lớp ModelNotification để định nghĩa thông báo
-   
 }
